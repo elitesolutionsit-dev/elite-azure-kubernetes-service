6a95190534127bfe9da80e6b86b7a0626182ddb5
elitesolutionit <elitesolutionsit@outlook.com>
2022-08-06 17:29:21 -0500

    adding conf file

--- config.tf
+++ config.tf
@@ -3,10 +3,9 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=2.59.0"
-
-      random = {
-        source = "hashicorp/random"
-      }
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
   required_version = ">= 0.13"
