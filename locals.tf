32171c4ee595e72b727e862c7795a02ea91d4c9d
elitesolutionit <elitesolutionsit@outlook.com>
2022-08-06 17:01:56 -0500

    rules for validation

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Service     = "devOps"
+    Owner       = "elitesolutionsit"
+    environment = var.environment
+    ManagedWith = "terraform"
+
+  }
+  
+  buildregion     = var.buildregion
+  SubcriptionName = var.SubcriptionName
+}
