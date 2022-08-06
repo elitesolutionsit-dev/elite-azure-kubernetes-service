a5058e80811f1a313b741b7988cd8656a79f6b69
elitesolutionit <elitesolutionsit@outlook.com>
2022-08-06 18:48:00 -0500

    adding autogenerate passwd for sp

--- aks.tf
+++ aks.tf
@@ -6,11 +6,6 @@ resource "azuread_service_principal" "eliteclusterdemodev-SP" {
   application_id = azuread_application.eliteclusterdemodev.application_id
 }
 
-resource "random_password" "eliteclusterdemodev-password" {
-  length  = 20
-  special = false
-}
 resource "azuread_service_principal_password" "eliteclusterdemodev-SP" {
   service_principal_id = azuread_service_principal.eliteclusterdemodev-SP.id
-  value = random_password.eliteclusterdemodev-password.result
 }
